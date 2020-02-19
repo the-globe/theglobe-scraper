e197cd03b217e68e93cd8a86b0616bac2b410204
Marvin Willms <marvin.willms@code.berlin>
2020-02-19 13:36:58 +0100

    added requierements.txt / established setup.py (used with the first release)

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
 # This is for a feature package!
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scraper",
+    version="0.0.1",
+    author="Santiago Martinez-Avial, Marvin Willms",
+    author_email="santiago.martinez@code.berlin, marvin.willms@code.berlin",
+    description="A package for scraping and inserting News to a MongoDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mavial/theglobescraper",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pymongo >= 3.10.1',
+        'feedparser >= 5.2.1',
+        'colorlog >= 4.1.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        # "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7.4',
+)
